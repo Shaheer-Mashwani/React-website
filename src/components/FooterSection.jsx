044dd18b96ea673e88621dc8f9f86fa7fac0955a
Shaheer-Mashwani <imsaalhusain@gmail.com>
2025-05-05 22:58:52 +0500

    name changed

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -68,7 +68,7 @@ const FooterSection = () => {
             <div className='border-t border-gray-200 mt-12 pt-8'>
                 <div className="flex flex-col md:flex-row justify-between items-center gap-4">
                     <p className="text-gray-600 text-sm">Copyright © {new Date().getFullYear()} codetutorbd.com</p>
-                    <p className="text-gray-600 text-sm">Created by Md Al Mamun</p>
+                    <p className="text-gray-600 text-sm">Created by Muhammad Shaheer</p>
                 </div>
             </div>
 
